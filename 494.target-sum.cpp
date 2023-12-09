3e0044164dc7702ea7e936c4fa0c106e67e44ca2
Vansh <vanshayushi@gmail.com>
2023-12-09 13:01:10 +0530

    lotd

--- 494.target-sum.cpp
+++ 494.target-sum.cpp
@@ -7,25 +7,36 @@
 // @lc code=start
 class Solution {
 public:
-    int solve(vector<int> nums,int n,int t,vector<vector<int>> dp){
-        if(n==0){
-            if(nums[0] == 0 && t==0) return 2;
-            if(nums[0] == t || sum==0) return 1;
+    int f(int index, int target, vector<int> &nums, vector<vector<int>> &dp){
+        if (index==0) {
+            if (target==0 && nums[index]==0) return 2;
+            if (nums[index]==target || target==0) return 1;
             return 0;
-        } 
-        if(dp[n][t]!=-1) return dp[n][t];
-        int take=0,nott=0;
-        if(nums[n]>=t) take = solve(nums,n-1,t-nums[n],dp);
-        nott = solve(nums,n-1,t,dp);
-        return nott+take;
+        }
+        if(dp[index][target]!=-1) return dp[index][target];
+        int Pick=0;
+        if (nums[index]<=target) Pick=f(index-1, target-nums[index], nums, dp);
+        int notPick=f(index-1, target, nums, dp);
+        return dp[index][target] = Pick+notPick;
+
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size(); int sum=0;
-        for(int i=0;i<n;i++) sum+=nums[i];
-        if((sum+target)%2!=0) return 0;
-        sum = (sum+target)/2;
-        vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-        return solve(nums,n-1,sum,dp);
+        int sum=accumulate(nums.begin(), nums.end(), 0);
+        int k=(sum-target);
+        if (k<0 || k%2!=0){
+            return 0;
+        }
+        int n=nums.size();
+        vector<vector<int>> dp(n, vector<int> (k+1, -1));
+        int ans= f(n-1, k/2, nums, dp);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<k+1;j++){
+                cout<<dp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return ans;
+        // return tab(n, k/2, nums);
     }
 };
 // @lc code=end
